5d8c9e116d442fcf946afe15ccb805521a200b93
kauan araujo <kauanraujo340@gmail.com>
2024-10-29 22:53:27 -0300

    finalizando projeto

--- Exercicio_lane_page/dist/index.f75de5e1.js
+++ Exercicio_lane_page/dist/index.f75de5e1.js
@@ -1,18 +1,22 @@
 AOS.init();
-const dataEvento = new date("nov 18, 2024 18:00:00");
+const dataEvento = new Date("nov 18, 2024 18:00:00");
 const timeStampEvento = dataEvento.getTime();
 const contaTempo = setInterval(function() {
-    const hoje = new dataEvento();
+    const hoje = new Date();
     const timeStampAgora = hoje.getTime();
     const tempoAteOEvento = timeStampEvento - timeStampAgora;
     const diaEmMs = 86400000;
     const horaEmMs = 3600000;
     const minutoEmMs = 60000;
-    const diasAteOEvento = math.floor(tempoAteOEvento / diaEmMs);
-    const horasAteOEvento = math.floor(tempoAteOEvento % diaEmMs / horaEmMs);
-    const minutosAteOEvento = math.floor(tempoAteOEvento % horaEmMs / minutoEmMs);
-    const segundosAteOEvento = math.floor((tempoAteOEvento & minutoEmMs) / 1000);
+    const diasAteOEvento = Math.floor(tempoAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(tempoAteOEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(tempoAteOEvento % horaEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor((tempoAteOEvento & minutoEmMs) / 1000);
     document.getElementById("TimerEvent").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    if (tempoAteOEvento < 0) {
+        clearInterval(contaTempo);
+        document.getElementById("TimerEvent").innerHTML = "Opss a festa ja rolou!";
+    }
 }, 1000);
 
 //# sourceMappingURL=index.f75de5e1.js.map
--- Exercicio_lane_page/src/scripts/main.js
+++ Exercicio_lane_page/src/scripts/main.js
@@ -1,10 +1,10 @@
 AOS.init();
 
-const dataEvento = new date("nov 18, 2024 18:00:00");
+const dataEvento = new Date("nov 18, 2024 18:00:00");
 const timeStampEvento = dataEvento.getTime();
 
-const contaTempo = setInterval(function(){
-    const hoje = new dataEvento();
+const contaTempo = setInterval(function() {
+    const hoje = new Date();
     const timeStampAgora = hoje.getTime();
 
     const tempoAteOEvento = timeStampEvento - timeStampAgora;
@@ -14,10 +14,16 @@ const contaTempo = setInterval(function(){
     const minutoEmMs = 1000 * 60;
 
 
-    const diasAteOEvento = math.floor(tempoAteOEvento / diaEmMs);
-    const horasAteOEvento = math.floor((tempoAteOEvento % diaEmMs) / horaEmMs);
-    const minutosAteOEvento = math.floor((tempoAteOEvento % horaEmMs) / minutoEmMs);
-    const segundosAteOEvento = math.floor((tempoAteOEvento & minutoEmMs) / 1000);
+    const diasAteOEvento = Math.floor(tempoAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((tempoAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((tempoAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((tempoAteOEvento & minutoEmMs) / 1000);
 
     document.getElementById('TimerEvent').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (tempoAteOEvento < 0){
+        clearInterval(contaTempo);
+        document.getElementById('TimerEvent').innerHTML = 'Opss a festa ja rolou!';
+    }
+
 },1000);
