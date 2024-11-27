f1f27a0319ec8b0e8a91a311a78b104e1fbb0ae9
kauan araujo <kauanraujo340@gmail.com>
2024-11-26 22:43:10 -0300

    finalizado

---
+++ exercicio_ts/src/main.js
@@ -0,0 +1,12 @@
+"use strict";
+let numero1 = 15;
+let numero2 = 87;
+let nome = "Luisa";
+function multiplicaNumeros(numero1, numero2) {
+    return numero1 * numero2;
+}
+function saudação(nome) {
+    return "olá " + nome;
+}
+console.log(multiplicaNumeros(numero1, numero2));
+console.log(saudação(nome));
