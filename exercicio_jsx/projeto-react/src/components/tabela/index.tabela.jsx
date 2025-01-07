67301d1d25732908d4a29f32dcec5bb4ce20ee9c
kauan araujo <kauanraujo340@gmail.com>
2025-01-06 21:55:09 -0300

    import ainda n aparece

--- exercicio_jsx/projeto-react/src/components/tabela/index.tabela.jsx
+++ exercicio_jsx/projeto-react/src/components/tabela/index.tabela.jsx
@@ -1,44 +1,48 @@
-const TabelaIMC=() => {
-    return(
-        <table>
+const Tabela = ({imc}) => {
+    return (
+        <>
+            <table className={styles.table__imc} cellSpacing={0} >
             <thead>
                 <tr>
-                    <th>
-                        Classificação
-                    </th>
-                    <th>
-                        IMC
-                    </th>
+                    <th colSpan={3}>Veja a interpretação do IMC</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>abaixo do peso</td>
-                    <td>abaixo de 18,5</td>
+                <tr className={styles.table__heading}>
+                    <td>imc</td>
+                    <td>classificação</td>
+                    <td>Obesidade (Grau)</td>
                 </tr>
-                <tr>
-                    <td>Peso normal</td>
-                    <td>Entre 18,5 e 24,9</td>
+                <tr id='magreza'>
+                <td>menor que 18,5</td>
+                <td>magreza</td>
+                <td>0</td>
                 </tr>
-                <tr>
-                    <td>Sobrepeso</td>
-                    <td>Entre 25 e 29,9</td>
+                <tr id='normal'>
+                <td>entre 18,5 e 24,9</td>
+                <td>normal</td>
+                <td>0</td>
                 </tr>
-                <tr>
-                    <td>Obesidade grau l</td>
-                    <td>Entre 30 e 34,9</td>
+                <tr id='sobrepeso'>
+                <td>entre 25,0 e 29,9</td>
+                <td>sobrepeso</td>
+                <td>I</td>
                 </tr>
-                <tr>
-                    <td>Obesidade grau ll</td>
-                    <td>Entre 35 e 39,9</td>
+                <tr id='obesidade'>
+                <td>entre 30,0 e 39,9</td>
+                <td>obesidade</td>
+                <td>II</td>
                 </tr>
-                <tr>
-                    <td>Obesidade grau lll</td>
-                    <td>Acima de 40</td>
+                <tr id='grave'>
+                <td>maior que 40,0</td>
+                <td>obesidade grave</td>
+                <td>III</td>
                 </tr>
             </tbody>
-        </table>
-    )
-}
+            </table>
+            <h2>seu imc: <span>{imc}</span></h2>
+        </>
+        )
+    }
 
 export default TabelaIMC;
