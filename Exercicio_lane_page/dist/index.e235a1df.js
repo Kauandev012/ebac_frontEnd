db468bb06aee5f35e13ce3e8bd76a51c9b09b8bf
kauan araujo <kauanraujo340@gmail.com>
2025-01-09 21:54:55 -0300

    erro branch

--- Exercicio_lane_page/dist/index.e235a1df.js
+++
@@ -1,2 +0,0 @@
-AOS.init();const e=new Date("nov 18, 2024 18:00:00").getTime(),t=setInterval(function(){let n=e-new Date().getTime(),o=Math.floor(n/864e5),a=Math.floor(n%864e5/36e5),r=Math.floor(n%36e5/6e4),l=Math.floor((6e4&n)/1e3);document.getElementById("TimerEvent").innerHTML=`${o}d ${a}h ${r}m ${l}s`,n<0&&(clearInterval(t),document.getElementById("TimerEvent").innerHTML="Opss a festa ja rolou!")},1e3);
-//# sourceMappingURL=index.e235a1df.js.map
--- exercicio_es6/src/main.js
+++
@@ -1,9 +0,0 @@
-const alunos = [{nome:'Ryan', nota: 5.5},{nome:'Luisa', nota: 8.1},{nome:'Pedro', nota:7.1},{nome:'Eloa',nota:5.9}];
-
-function filtraAlunos(alunos){
-    return alunos.nota >= 6 ;
-}
-
-const alunosMediaAlta = alunos.filter(filtraAlunos);
-
-
