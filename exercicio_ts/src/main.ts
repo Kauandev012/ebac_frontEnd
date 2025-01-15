ab328b22349c593218e98130b23340770ae8a9e8
kauan araujo <kauanraujo340@gmail.com>
2025-01-14 21:42:42 -0300

    commit

---
+++ exercicio_ts/src/main.ts
@@ -0,0 +1,15 @@
+let numero1 = 15;
+let numero2 = 87;
+let nome = "Luisa";
+
+function multiplicaNumeros (numero1 : number , numero2 :number ): number {
+    return numero1 * numero2;
+}
+
+function saudação (nome:string): string {
+    return "olá " + nome;
+}
+
+
+console.log(multiplicaNumeros(numero1,numero2));
+console.log(saudação(nome));
