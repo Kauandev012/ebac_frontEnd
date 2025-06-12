69e1f08668e30e62ae50ce017f28e51cecf3f293
kauan araujo <kauanraujo340@gmail.com>
2025-06-12 17:04:04 -0300

    commit validação

--- cypress/e2e/ebac-lista-contatos/alteracao.cy.js
+++ cypress/e2e/ebac-lista-contatos/alteracao.cy.js
@@ -10,9 +10,7 @@ describe('teste para alteração de contato', () => {
         cy.get('input[type="text"]').clear().type('gian')
         cy.get('input[type="email"]').clear().type('teste@gmail.com')
         cy.get('.alterar').click()
-    })
-
-    it('verificar se o elemento foi alterado', () =>{
         cy.get(':nth-child(2) > .sc-dmqHEX > .sc-eDDNvR > :nth-child(1)').should('have.text', 'gian')
+        cy.get(':nth-child(2) > .sc-dmqHEX > .sc-eDDNvR > :nth-child(3)').should('have.text', 'teste@gmail.com')
     })
 })
